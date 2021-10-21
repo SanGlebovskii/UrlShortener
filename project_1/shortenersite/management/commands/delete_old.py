cd4d3bfcb9ac6bf964f8e8d6f8172060e8d33f6b
KateKopteva <bychkokatya@gmail.com>
2021-10-21 15:53:06 +0300

    Initial commit

---
+++ project_1/shortenersite/management/commands/delete_old.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand, CommandError
+from shortenersite.models import Url
+from datetime import datetime, timedelta
+
+class Command(BaseCommand):
+    help = 'Удаление ссылок старше 3 дней'
+
+    def handle(self, *args, **options):
+        Url.objects.filter(created_at__lte=datetime.now()-timedelta(days=3)).delete()
+        self.stdout.write('Удалены ссылке старше 3 дней')
--- project_1/shortenersite/urls.py
+++ project_1/shortenersite/urls.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
+urlpatterns = [
+    path('', index, name='index'),
+    path('create', create, name='create'),
+    path('<str:pk>', go, name='go'),
+]
